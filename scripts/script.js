e0750c8f7ba6cb243892ff95c5331af2528ccad3
SieuwkeSheta <sieuwke_sheta@hotmail.nl>
2025-09-03 14:57:38 +0200

    Nieuwe png's en lay-out

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,33 @@
 /* Stap 1: Selecteer de knop */
-let expandButton = document.querySelector('.profile-card-example-button')
+// let expandButton = document.querySelector('.profile-card-example-button')
 
-/* Stap 2: Wacht tot iemand op die knop klikt */
-expandButton?.addEventListener('click', function() {
+// /* Stap 2: Wacht tot iemand op die knop klikt */
+// expandButton?.addEventListener('click', function() {
 
-  /* Stap 3: Verander een class zodra iemand op die knop klikt */
-  document.body.classList.toggle('is-expanded')
+//   /* Stap 3: Verander een class zodra iemand op die knop klikt */
+//   document.body.classList.toggle('is-expanded')
+// })
+
+let werkButton = document.querySelector('#werk')
+
+werkButton.addEventListener('click', function() {
+  console.log("werk");
+  document.querySelector(".werk1").classList.toggle('is-shown')
+  document.querySelector(".werk2").classList.toggle('is-shown')
+})
+
+let hobbiesButton = document.querySelector('#hobbies')
+
+hobbiesButton.addEventListener('click', function() {
+  console.log("hobbies");
+  document.querySelector(".hobbies1").classList.toggle('is-shown')
+  document.querySelector(".hobbies2").classList.toggle('is-shown')
+  document.querySelector(".hobbies3").classList.toggle('is-shown')
+})
+
+let ambitiesButton = document.querySelector('#ambities')
+
+ambitiesButton.addEventListener('click', function() {
+  console.log("ambities");
+  document.querySelector(".ambities").classList.toggle('is-shown')
 })
